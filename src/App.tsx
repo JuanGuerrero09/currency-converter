87027305bafbf63103939e4154e18de2eb96355a
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-02-16 15:59:34 -0500

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,51 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { CurrencyHolder } from "./components/CurrencyHolder";
-import { adjustValue } from "./services/adjustValues";
 import { ContextCurrency } from "./context/ContextValue";
+import Header from "./components/Header";
 
 function App() {
   const [activeCurrency, setActiveCurrency] = useState<string>("USD");
   const [activeValue, setActiveValue] = useState<number>(1);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const currentValue = adjustValue(event.target.value);
-    setActiveValue(currentValue);
-  };
-
-  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const optionSelected = e.target.value;
-    setActiveCurrency(optionSelected);
+    const currentValue = event.target.value;
+    setActiveValue(+currentValue);
   };
 
   const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
     const newActiveCurrency = (event.target.nextSibling as HTMLSelectElement)
       .value;
-    const newActiveValue = adjustValue(event.target.value);
+    const newActiveValue = event.target.value;
     setActiveCurrency(newActiveCurrency);
-    setActiveValue(newActiveValue);
+    setActiveValue(+newActiveValue);
   };
 
   return (
-    <div className="App">
-      <header>
-        <h1>Currency Converter</h1>
-      </header>
-      <main>
-        <form>
-          <ContextCurrency.Provider value={{
+    <div className="App relative h-screen w-screen overflow-y-hidden">
+      <Header />
+      <main className="flex flex-col items-center mt-8">
+        <ContextCurrency.Provider
+          value={{
             activeCurrency,
-            activeValue
-          }}>
-            <CurrencyHolder
-              handleChange={handleChange}
-              handleFocus={handleFocus}
-            />
-            <CurrencyHolder
-              handleChange={handleChange}
-              handleFocus={handleFocus}
-            />
-            <CurrencyHolder
-              handleChange={handleChange}
-              handleFocus={handleFocus}
-            />
-          </ContextCurrency.Provider>
-        </form>
+            activeValue,
+          }}
+        >
+          <CurrencyHolder
+            handleChange={handleChange}
+            handleFocus={handleFocus}
+          />
+          <CurrencyHolder
+            handleChange={handleChange}
+            handleFocus={handleFocus}
+          />
+          <CurrencyHolder
+            handleChange={handleChange}
+            handleFocus={handleFocus}
+          />
+        </ContextCurrency.Provider>
       </main>
-      <footer>Juan David</footer>
+      <footer className="absolute bottom-2 w-screen flex justify-center">Juan David</footer>
     </div>
   );
 }
