f7170fc25cbec0558de52497f6b21e7b3378f70c
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-02-16 16:00:07 -0500

    Header component created

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+        <h1 className='min-w-full font-semibold flex justify-center'>Currency Converter</h1>
+    </>
+  )
+}
