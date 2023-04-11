b8c00119b47c7c7bb60f31badcee97cbe4c54f48
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-04-11 14:07:18 -0500

    deploy app

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,12 @@ function App() {
   return (
     <div className="App relative h-screen w-screen overflow-y-hidden">
       <Header />
-      <main className="flex flex-col items-center mt-8">
+      <main className="flex flex-col items-center mt-8 ">
+        <p>Simple and real-time currency converter</p>
+        <p className="mb-8">Powered by: <a href="https://rapidapi.com/exchangerateapi/api/exchangerate-api/">ExchangeRate-API</a></p>
         <ContextProvider>
+          <CurrencyHolder />
+          <CurrencyHolder />
           <CurrencyHolder />
           <CurrencyHolder />
           <CurrencyHolder />
